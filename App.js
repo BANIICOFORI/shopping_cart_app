d14c4b86ee4a95fd867fab3ee1cc5aeb20b441eb
BANIICOFORI <74446525+BANIICOFORI@users.noreply.github.com>
2023-03-16 15:44:00 +0000

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AddToCart from './components/AddToCart';
+import Payment from './components/Payment';
+import Shopping from './components/Shopping';
+import WomensJacket from './components/WomensJacket';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <Shopping />
+      {/* <AddToCart /> */}
+      {/* <Payment /> */}
+      {/* <WomensJacket/> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+   flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ components/WomensJacket.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+
+const WomensJacketList = () => {
+    
+  return (
+      <View style={styles.container}>
+          <View style={styles.Heading}>
+              <Text style={styles.HeadingText}>Womens Jacket</Text>
+          </View>
+          <View>
+              <View>
+                  <Image style={styles.Image} source={require("../assets/add_to_cart.jpg")} />
+              </View>
+          </View>
+      </View>
+  )
+}
+
+export default WomensJacketList;
+
+const styles = StyleSheet.create({
+  container:{
+    padding: 10,
+    justifycontent:"center",
+  },
+  Image: {
+    width: 200,
+    height: 150,
+    marginHorizontal: 10,
+    marginVertical: 20,
+    alignSelf: "flex-start",
+ 
+},
+Heading: {
+    marginHorizontal: 10,
+    marginTop:80,
+    marginBottom:20,
+
+},
+HeadingText:{
+    fontSize:30,
+    fontWeight:"bold",
+    marginHorizontal:20,
+
+},
+});
